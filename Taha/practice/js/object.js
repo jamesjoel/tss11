06061445107a03b3ec0f4397cacad8ea07092e16
tahajaffri123 <tahajaffri87@gmail.com>
2022-06-16 15:31:10 +0530

    navbar and js practice+task

---
+++ Taha/practice/js/object.js
@@ -0,0 +1,37 @@
+// var a=10;
+// console.log(a);
+// var a=20,b=10
+// // console.log(a+b);
+// var arr=[5,6,4,3,7,2,1,5,67,234,11,33]
+// console.log(arr[4]);
+// var arr1={name:"taha",age:22}
+// console.log(arr1.age);
+var car=[
+    {
+        name:"ferrari",
+        color:"Red",
+        price:2500000
+    },
+    {
+        name:"maruti800",
+        color:"white",
+        price:500000
+    },
+    {
+        name:"audia4",
+        color:"grey",
+        price:2150000
+    },
+    {
+        name:"lamborghini",
+        type:["avetador","gladiator","spider"],
+        color:"Red",
+        price:62500000
+    },
+    {
+        name:"toyota",
+        color:"Red",
+        price:500000
+    }
+]
+console.log(car[3].type[0]);
---
+++ Taha/practice/object2.js
@@ -0,0 +1,7 @@
+var a=10;
+var b=20;
+var arr=[1,2,3,4,5,6];
+var arr3=["taha",arr,"taher"];
+var arr2=[55,33,44,arr3,66];
+
+console.log(arr2[3][1][3]);
