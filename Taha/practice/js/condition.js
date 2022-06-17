3ad0d800b472c196fd584875fd6fad93322e0683
tahajaffri123 <tahajaffri87@gmail.com>
2022-06-17 14:44:28 +0530

    conditions practice

---
+++ Taha/practice/js/condition.js
@@ -0,0 +1,44 @@
+// var a=10;
+// a%2==0?console.log("even") : console.log("odd");
+// if(a%2==0){console.log("Even");}
+// console.log("odd");
+// var student=[
+//     {
+//         name:"rohan",
+//         class:"10",
+//         mobileNo:783782
+//     },
+//     {
+//         name:"rakesh",
+//         class:"10",
+//         mobileNo:783782234
+//     },
+//     {
+//         name:"jay",
+//         class:"10",
+//         mobileNo:783
+//     },
+//     {
+//         name:"joe",
+//         class:"10",
+//         mobileNo:78332413782
+//     },
+//     {
+//         name:"monty",
+//         class:"10",
+//         mobileNo:7837822323
+//     }
+
+// ]
+// for(var x=0 ; x>student[5] ; x++){
+    
+//     console.log(student[x]);
+// }
+// const numbers = [45, 4, 9, 16, 25];
+// let txt = "";
+// student.forEach(myFunction);
+
+// function myFunction(value, index, array) {
+//   txt += value + "<br>";
+// }
+// console.log(txt);
