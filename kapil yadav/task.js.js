37a74394b0df5357f325dfad904145783bb90d90
kapilyada <kapilyada01@gmail.com>
2022-06-22 11:39:24 +0530

    task

---
+++ kapil yadav/task.js.js
@@ -0,0 +1,614 @@
+function  marksheet(m,s,e,a)
+ {
+    var x = m+s+e;
+    var y = x*100/300;
+    var r =a*100/365;
+    var p = r;
+     
+
+    if( y>80 && p>80)
+    
+    { 
+        console.log( "excllent");
+
+// kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk
+    }
+    
+if ( y>=80 && p<80 && p<70 && p<60 && p<50 )
+{
+    console.log("fail");
+}
+if ( y>=80 && p<80 && p<70 && p<60 )
+{
+    console.log("pass");
+}
+if ( y>=80 && p<80 && p<70 )
+{
+    console.log(" good");
+}
+if ( y>=80 && p<80  )
+{
+    console.log("very good");
+}
+
+
+if(  y>=70 &&   p<70 && p<60 && p<50       )
+{
+    console.log("fail");
+}
+if(  y>=70 &&   p<70 && p<60        )
+{
+    console.log("pass");
+}
+
+if(  y>=70 &&   p<70        )
+{
+    console.log("good");
+}
+
+
+if( y>=70 && p<80)
+{
+    console.log("very good")
+}
+
+
+if(  y>=70 && p>=80 )
+{
+    console.log("exsilent")
+}
+
+
+if( y>=60 && p<60 && p<50    )
+{
+    console.log("fail");
+}
+
+if( y>=60 && p<60     )
+{
+    console.log("pass");
+}
+if( y>=60 && p>70  && p>80   )
+{
+    console.log("very good");
+}
+if( y>=60 && p>70    )
+{
+    console.log("good");
+}
+
+
+if(y>=50 && p<50  )
+{
+    console.log("fail")
+}
+if( y>=50 && p<60    )
+{
+    console.log("pass");
+}
+if( y>=50 && p>60  )
+{
+    console.log("pass");
+}
+if(y>=50 && p>70    )
+{
+console.log("good");
+ }
+if( y>=50 && p>80  )
+{
+    console.log("very good");
+}
+
+
+if(y<50 && p<50        )
+{
+    console.log("fail");
+}
+if( y<50 && p>50            )
+{
+    console.log("fail");
+}
+if(  y<50 && p>60     )
+{
+    console.log("fail");
+}
+if(y<50 && p>70     )
+{
+    console.log("fail");
+}
+if( y<50 && p>80      )
+{
+    console.log("fail");
+}
+if ( y<50 && p<80     )
+{
+    console.log("fail");
+}
+
+
+
+    
+        
+    
+    if ( y>70 &&  y<=80  && p>70 && p<=80   )
+    {
+        console.log("very good");
+        // kkkkkkkkkkkkkkkkkkkkkkkkkkkkkk
+    }
+    
+if ( y>=80 && p<80 && p<70 && p<60 && p<50 )
+{
+    console.log("fail");
+}
+if ( y>=80 && p<80 && p<70 && p<60 )
+{
+    console.log("pass");
+}
+if ( y>=80 && p<80 && p<70 )
+{
+    console.log(" good");
+}
+if ( y>=80 && p<80  )
+{
+    console.log("very good");
+}
+
+
+if(  y>=70 &&   p<70 && p<60 && p<50       )
+{
+    console.log("fail");
+}
+if(  y>=70 &&   p<70 && p<60        )
+{
+    console.log("pass");
+}
+
+if(  y>=70 &&   p<70        )
+{
+    console.log("good");
+}
+
+
+if( y>=70 && p<80)
+{
+    console.log("very good")
+}
+
+
+if(  y>=70 && p>=80 )
+{
+    console.log("exsilent")
+}
+
+
+if( y>=60 && p<60 && p<50    )
+{
+    console.log("fail");
+}
+
+if( y>=60 && p<60     )
+{
+    console.log("pass");
+}
+if( y>=60 && p>70  && p>80   )
+{
+    console.log("very good");
+}
+if( y>=60 && p>70    )
+{
+    console.log("good");
+}
+
+
+if(y>=50 && p<50  )
+{
+    console.log("fail")
+}
+if( y>=50 && p<60    )
+{
+    console.log("pass");
+}
+if( y>=50 && p>60  )
+{
+    console.log("pass");
+}
+if(y>=50 && p>70    )
+{
+console.log("good");
+ }
+if( y>=50 && p>80  )
+{
+    console.log("very good");
+}
+
+
+if(y<50 && p<50        )
+{
+    console.log("fail");
+}
+if( y<50 && p>50            )
+{
+    console.log("fail");
+}
+if(  y<50 && p>60     )
+{
+    console.log("fail");
+}
+if(y<50 && p>70     )
+{
+    console.log("fail");
+}
+if( y<50 && p>80      )
+{
+    console.log("fail");
+}
+if ( y<50 && p<80     )
+{
+    console.log("fail");
+}
+
+
+
+    if (y>60 && y<=70 && p>60 && p<=70      )
+    {
+        console.log("good ");
+        // kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk
+    }
+    
+if ( y>=80 && p<80 && p<70 && p<60 && p<50 )
+{
+    console.log("fail");
+}
+if ( y>=80 && p<80 && p<70 && p<60 )
+{
+    console.log("pass");
+}
+if ( y>=80 && p<80 && p<70 )
+{
+    console.log(" good");
+}
+if ( y>=80 && p<80  )
+{
+    console.log("very good");
+}
+
+
+if(  y>=70 &&   p<70 && p<60 && p<50       )
+{
+    console.log("fail");
+}
+if(  y>=70 &&   p<70 && p<60        )
+{
+    console.log("pass");
+}
+
+if(  y>=70 &&   p<70        )
+{
+    console.log("good");
+}
+
+
+if( y>=70 && p<80)
+{
+    console.log("very good")
+}
+
+
+if(  y>=70 && p>=80 )
+{
+    console.log("exsilent")
+}
+
+
+if( y>=60 && p<60 && p<50    )
+{
+    console.log("fail");
+}
+
+if( y>=60 && p<60     )
+{
+    console.log("pass");
+}
+if( y>=60 && p>70  && p>80   )
+{
+    console.log("very good");
+}
+if( y>=60 && p>70    )
+{
+    console.log("good");
+}
+
+
+if(y>=50 && p<50  )
+{
+    console.log("fail")
+}
+if( y>=50 && p<60    )
+{
+    console.log("pass");
+}
+if( y>=50 && p>60  )
+{
+    console.log("pass");
+}
+if(y>=50 && p>70    )
+{
+console.log("good");
+ }
+if( y>=50 && p>80  )
+{
+    console.log("very good");
+}
+
+
+if(y<50 && p<50        )
+{
+    console.log("fail");
+}
+if( y<50 && p>50            )
+{
+    console.log("fail");
+}
+if(  y<50 && p>60     )
+{
+    console.log("fail");
+}
+if(y<50 && p>70     )
+{
+    console.log("fail");
+}
+if( y<50 && p>80      )
+{
+    console.log("fail");
+}
+if ( y<50 && p<80     )
+{
+    console.log("fail");
+}
+
+
+
+    if (y>50 && y<=60 && p>50 && p<=60      )
+    {
+        console.log("pass");
+        // kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk
+    
+    }
+    
+if ( y>=80 && p<80 && p<70 && p<60 && p<50 )
+{
+    console.log("fail");
+}
+if ( y>=80 && p<80 && p<70 && p<60 )
+{
+    console.log("pass");
+}
+if ( y>=80 && p<80 && p<70 )
+{
+    console.log(" good");
+}
+if ( y>=80 && p<80  )
+{
+    console.log("very good");
+}
+
+
+if(  y>=70 &&   p<70 && p<60 && p<50       )
+{
+    console.log("fail");
+}
+if(  y>=70 &&   p<70 && p<60        )
+{
+    console.log("pass");
+}
+
+if(  y>=70 &&   p<70        )
+{
+    console.log("good");
+}
+
+
+if( y>=70 && p<80)
+{
+    console.log("very good")
+}
+
+
+if(  y>=70 && p>=80 )
+{
+    console.log("exsilent")
+}
+
+
+if( y>=60 && p<60 && p<50    )
+{
+    console.log("fail");
+}
+
+if( y>=60 && p<60     )
+{
+    console.log("pass");
+}
+if( y>=60 && p>70  && p>80   )
+{
+    console.log("very good");
+}
+if( y>=60 && p>70    )
+{
+    console.log("good");
+}
+
+
+if(y>=50 && p<50  )
+{
+    console.log("fail")
+}
+if( y>=50 && p<60    )
+{
+    console.log("pass");
+}
+if( y>=50 && p>60  )
+{
+    console.log("pass");
+}
+if(y>=50 && p>70    )
+{
+console.log("good");
+ }
+if( y>=50 && p>80  )
+{
+    console.log("very good");
+}
+
+
+if(y<50 && p<50        )
+{
+    console.log("fail");
+}
+if( y<50 && p>50            )
+{
+    console.log("fail");
+}
+if(  y<50 && p>60     )
+{
+    console.log("fail");
+}
+if(y<50 && p>70     )
+{
+    console.log("fail");
+}
+if( y<50 && p>80      )
+{
+    console.log("fail");
+}
+if ( y<50 && p<80     )
+{
+    console.log("fail");
+}
+
+
+
+    if (y<50 && p<50)
+    {
+        console.log("fail");
+        // kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk
+    }
+    
+if ( y>=80 && p<80 && p<70 && p<60 && p<50 )
+{
+    console.log("fail");
+}
+if ( y>=80 && p<80 && p<70 && p<60 )
+{
+    console.log("pass");
+}
+if ( y>=80 && p<80 && p<70 )
+{
+    console.log(" good");
+}
+if ( y>=80 && p<80  )
+{
+    console.log("very good");
+}
+
+
+if(  y>=70 &&   p<70 && p<60 && p<50       )
+{
+    console.log("fail");
+}
+if(  y>=70 &&   p<70 && p<60        )
+{
+    console.log("pass");
+}
+
+if(  y>=70 &&   p<70        )
+{
+    console.log("good");
+}
+
+
+if( y>=70 && p<80)
+{
+    console.log("very good")
+}
+
+
+if(  y>=70 && p>=80 )
+{
+    console.log("exsilent")
+}
+
+
+if( y>=60 && p<60 && p<50    )
+{
+    console.log("fail");
+}
+
+if( y>=60 && p<60     )
+{
+    console.log("pass");
+}
+if( y>=60 && p>70  && p>80   )
+{
+    console.log("very good");
+}
+if( y>=60 && p>70    )
+{
+    console.log("good");
+}
+
+
+if(y>=50 && p<50  )
+{
+    console.log("fail")
+}
+if( y>=50 && p<60    )
+{
+    console.log("pass");
+}
+if( y>=50 && p>60  )
+{
+    console.log("pass");
+}
+if(y>=50 && p>70    )
+{
+console.log("good");
+ }
+if( y>=50 && p>80  )
+{
+    console.log("very good");
+}
+
+
+if(y<50 && p<50        )
+{
+    console.log("fail");
+}
+if( y<50 && p>50            )
+{
+    console.log("fail");
+}
+if(  y<50 && p>60     )
+{
+    console.log("fail");
+}
+if(y<50 && p>70     )
+{
+    console.log("fail");
+}
+if( y<50 && p>80      )
+{
+    console.log("fail");
+}
+if ( y<50 && p<80     )
+{
+    console.log("fail");
+}
+
+
+
+    
+    
+    
+}
+marksheet( 75,75,75,140)
