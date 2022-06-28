f03daaa0e2f8e38d1a1b2b42fb8c0b283750d822
jamesjoel <jamesjoelj@gmail.com>
2022-06-28 12:20:04 +0530

    express day 4

---
+++ exp5/app.js
@@ -0,0 +1,48 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+// app.set("views", ["nahid", "taha"]);
+
+app.use(express.static(__dirname+"/assets"));
+
+app.get("/", (req, res)=>{
+
+    var pagedata = { pagename : "home", title : "Home Page", nav : "navbar" }
+    res.render("layout", pagedata);
+})
+
+
+
+
+app.get("/about", (req, res)=>{
+
+
+    var pagedata = { pagename : "about", title : "About Page", nav : "navbar"  }
+    res.render("layout", pagedata);
+})
+
+
+app.get("/contact", (req, res)=>{
+
+
+    var pagedata = { pagename : "contact", title : "Contact Page", nav : "navbar2"  }
+    res.render("layout", pagedata);
+})
+
+
+
+
+var port = process.env.PORT || 3000;
+app.listen(port, ()=>{
+    console.log("server running");
+})
+/*
+    res.send() ----------------- send data
+
+    res.sendFile() ----------------- html file send
+
+    res.render() ----------------- ejs file send and data also
+
+
+*/
