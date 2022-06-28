f714d5b16c4ea304da2ccca4f7fee7ba3da3de9c
tahajaffri123 <tahajaffri87@gmail.com>
2022-06-28 11:23:50 +0530

    temp1+2 dynamic

---
+++ Taha/template 1/app.js
@@ -0,0 +1,34 @@
+var express=require("express");
+var app = express();
+app.set("view engine","ejs");
+app.use(express.static(__dirname+"/public"));
+
+app.get("/",(req,res)=>{
+    // res.sendFile(__dirname+"/home.html");
+    var name="Taha";
+    var collage="IPS ACADEMY";
+    var mobno_="8770547897";
+
+    var arr=[name,23,collage,mobno_];
+    var pagedata={a:arr};
+    res.render("home",pagedata);
+});
+
+
+app.get("/about",(req,res)=>{
+    // res.sendFile(__dirname+"/about.html");
+    res.render("about");
+});
+app.get("/contact",(req,res)=>{
+    // res.sendFile(__dirname+"/contact.html");
+    res.render("contact");
+});
+app.get("/iframe",(req,res)=>{
+    // res.sendFile(__dirname+"/contact.html");
+    res.render("contact");
+});
+
+
+app.listen(4000,()=>{
+    console.log("server_running");
+})
