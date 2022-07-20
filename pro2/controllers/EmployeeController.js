90b270cceea789ccf854a07a92baae46054ceb9d
jamesjoel <jamesjoelj@gmail.com>
2022-07-20 12:53:35 +0530

    crud 2

--- pro2/controllers/EmployeeController.js
+++ pro2/controllers/EmployeeController.js
@@ -1,6 +1,7 @@
 const routes = require("express").Router();
 const Employee = require("../models/Employee");
 const City = require("../models/City");
+const mongodb = require("mongodb");
 
 routes.get("/", (req, res)=>{
     Employee.find({}, (err, result)=>{
@@ -16,6 +17,29 @@ routes.get("/", (req, res)=>{
 routes.post("/", (req, res)=>{
     // console.log(req.body);
     Employee.insert(req.body, (err)=>{
+        res.redirect("/employee");
+    })
+})
+
+routes.get("/delete/:id", (req, res)=>{
+    var objid = mongodb.ObjectId(req.params.id);
+    Employee.delete({ _id : objid }, (err)=>{
+        res.redirect("/employee");
+    })
+})
+
+routes.get("/edit/:id", (req, res)=>{
+    var objid = mongodb.ObjectId(req.params.id);
+    Employee.find({ _id : objid }, (err, result)=>{
+        City.find({}, (err, city)=>{
+            var pagedata = { pagename : "employee/edit", result : result[0], city : city };
+            res.render("layout", pagedata);
+        });
+    })
+})
+routes.post("/update/:id", (req, res)=>{
+    var objid = mongodb.ObjectId(req.params.id);
+    Employee.update({ _id : objid }, req.body, (err)=>{
         res.redirect("/employee");
     })
 })
---
+++ pro2/controllers/TeacherController.js
@@ -0,0 +1,9 @@
+const routes = require("express").Router();
+
+routes.get("/", (req, res)=>{
+    var pagedata = { pagename : "about/index"};
+    res.render("layout", pagedata);
+});
+
+
+module.exports = routes;
